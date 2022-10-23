86eaf3a1395c4970e5fe89aa584e38d4785672c7
adammkeane <adamkeane.edu@gmail.com>
2022-10-23 19:31:50 +0000

    Add autocomplete function for answer input

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,4 +26,67 @@
     Maldives : 'Male',
     Mongolia : 'Ulaanbaatar',
     Myanmar : 'Naypyidaw'
+ };
+
+
+
+
+
+ // reference
+ let ansInput = document.getElementById('answer');
+ let ansSubmit = document.getElementById('submit-button');
+
+
+
+ ansInput.addEventListener('keyup', (e) => {
+   //loop through above array
+  //Initially remove all elements ( so if user erases a letter or adds new letter then clean previous outputs)
+  removeElements();
+  for (let i in countryList) {
+    //convert input to lowercase and compare with each string
+    if (
+      countryList[i].toLowerCase().startsWith(ansInput.value.toLowerCase()) &&
+      ansInput.value != ""
+    ) {
+      //create li element
+      let listItem = document.createElement("li");
+      //One common class name
+      listItem.classList.add("options-list-items");
+      listItem.style.cursor = "pointer";
+      listItem.setAttribute("onclick", "displayNames('" + countryList[i] + "')");
+      //Display matched part in bold
+      let word = "<b>" + countryList[i].substr(0, ansInput.value.length) + "</b>";
+      word += countryList[i].substr(ansInput.value.length);
+      //display the value in array
+      listItem.innerHTML = word;
+      document.querySelector(".options-list").appendChild(listItem);
+    }
+  }
+ });
+
+ function displayNames(value) {
+   ansInput.value = value;
+   removeElements();
  }
+ function removeElements() {
+   //clear all the item
+   let items = document.querySelectorAll(".options-list-items");
+   items.forEach((item) => {
+     item.remove();
+   });
+ }
+
+ 
+ //only allow real capital cities from the object as options
+ function checkAns (event) {
+   event.preventDefault();
+   if (!(Object.values(countryList).includes(ansInput.value))) {
+       alert('Not a valid option');
+   }
+ };
+
+ //check users answer
+ ansSubmit.addEventListener('click', checkAns);
+
+ //randomly generate a country
+ 
