6204f63310a1835b512799406e385e0b330cd8b7
adammkeane <adamkeane.edu@gmail.com>
2022-10-23 22:23:15 +0000

    Add refresh question functionality after a guess

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,97 +1,115 @@
- let countryList = {
-    Afghanistan : 'Kabul',
-    Armenia : 'Yerevan',
-    Azerbaijan : 'Baku',
-    Bahrain : 'Manama',
-    Bangladesh : 'Dhaka',
-    Bhutan : 'Thimphu',
-    Brunei : 'Bandar Seri Begawan',
-    Cambodia : 'Phnom Penh',
-    China : 'Beijing',
-    Cyprus : 'Nicosia',
-    Georgia : 'Tbilisi',
-    India : 'New Dehli',
-    Indonesia : 'Jakarta',
-    Iran : 'Tehran',
-    Iraq : 'Baghdad',
-    Israel : 'Jerusalem',
-    Japan : 'Toyko',
-    Jordan : 'Amman',
-    Kazakhstan : 'Astana',
-    Kuwait : 'Kuwait City',
-    Kyrgyzstan : 'Bishkek',
-    Laos : 'Vietiane',
-    Lebanon : 'Beirut',
-    Malaysia : 'Kuala Lumpur',
-    Maldives : 'Male',
-    Mongolia : 'Ulaanbaatar',
-    Myanmar : 'Naypyidaw'
- };
+let countryList = {
+  Afghanistan : 'Kabul',
+  Armenia : 'Yerevan',
+  Azerbaijan : 'Baku',
+  Bahrain : 'Manama',
+  Bangladesh : 'Dhaka',
+  Bhutan : 'Thimphu',
+  Brunei : 'Bandar Seri Begawan',
+  Cambodia : 'Phnom Penh',
+  China : 'Beijing',
+  Cyprus : 'Nicosia',
+  Georgia : 'Tbilisi',
+  India : 'New Dehli',
+  Indonesia : 'Jakarta',
+  Iran : 'Tehran',
+  Iraq : 'Baghdad',
+  Israel : 'Jerusalem',
+  Japan : 'Toyko',
+  Jordan : 'Amman',
+  Kazakhstan : 'Astana',
+  Kuwait : 'Kuwait City',
+  Kyrgyzstan : 'Bishkek',
+  Laos : 'Vietiane',
+  Lebanon : 'Beirut',
+  Malaysia : 'Kuala Lumpur',
+  Maldives : 'Male',
+  Mongolia : 'Ulaanbaatar',
+  Myanmar : 'Naypyidaw'
+};
 
- document.addEventListener("DOMContentLoaded", function() {
-   generateCountry();
+//create random numbers between 0 and number of country key indexes
+let num1 = Math.floor(Math.random() * (Object.keys(countryList).length - 1));
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  //check users answer
+  ansSubmit.addEventListener('click', checkAns);
+  runGame(num1);
 });
 
- // reference
- let ansInput = document.getElementById('answer');
- let ansSubmit = document.getElementById('submit-button');
- let countryQ = document.getElementById('country');
-   //create random numbers between 0 and number of country key indexes
-   let num1 = Math.floor(Math.random() * (Object.keys(countryList).length - 1));
+// reference
+let ansInput = document.getElementById('answer');
+let ansSubmit = document.getElementById('submit-button');
+let countryQ = document.getElementById('country');
 
- ansInput.addEventListener('keyup', (e) => {
-   //loop through above array
-  //Initially remove all elements ( so if user erases a letter or adds new letter then clean previous outputs)
-  removeElements();
-  for (let i in countryList) {
-    //convert input to lowercase and compare with each string
-    if (
-      countryList[i].toLowerCase().startsWith(ansInput.value.toLowerCase()) &&
-      ansInput.value != ""
-    ) {
-      //create li element
-      let listItem = document.createElement("li");
-      //One common class name
-      listItem.classList.add("options-list-items");
-      listItem.style.cursor = "pointer";
-      listItem.setAttribute("onclick", "displayNames('" + countryList[i] + "')");
-      //Display matched part in bold
-      let word = "<b>" + countryList[i].substr(0, ansInput.value.length) + "</b>";
-      word += countryList[i].substr(ansInput.value.length);
-      //display the value in array
-      listItem.innerHTML = word;
-      document.querySelector(".options-list").appendChild(listItem);
-    }
+
+
+
+ansInput.addEventListener('keyup', (e) => {
+//Initially remove all elements ( so if user erases a letter or adds new letter then clean previous outputs)
+removeElements();
+//loop through above array
+for (let i in countryList) {
+  //convert input to lowercase and compare with each string
+  if (
+    countryList[i].toLowerCase().startsWith(ansInput.value.toLowerCase()) &&
+    ansInput.value != ""
+  ) {
+    //create li element
+    let listItem = document.createElement("li");
+    //One common class name
+    listItem.classList.add("options-list-items");
+    listItem.style.cursor = "pointer";
+    listItem.setAttribute("onclick", "displayNames('" + countryList[i] + "')");
+    //Display matched part in bold
+    let word = "<b>" + countryList[i].substr(0, ansInput.value.length) + "</b>";
+    word += countryList[i].substr(ansInput.value.length);
+    //display the value in array
+    listItem.innerHTML = word;
+    document.querySelector(".options-list").appendChild(listItem);
   }
- });
+}
+});
 
- function displayNames(value) {
-   ansInput.value = value;
-   removeElements();
- }
- function removeElements() {
-   //clear all the item
-   let items = document.querySelectorAll(".options-list-items");
-   items.forEach((item) => {
-     item.remove();
-   });
- }
- 
- //functio to check users answers
- function checkAns (event) {
-   event.preventDefault();
-   //only allow real capital cities from the object as options and check if answer correct.
-   if (!(Object.values(countryList).includes(ansInput.value))) {
-       alert('Sorry, not a valid option. Please section an answer from the dropdown list. Start typing your answer to bring up the dropdown menu.');
-   } 
- };
+function displayNames(value) {
+  ansInput.value = value;
+  removeElements();
+}
+function removeElements() {
+  //clear all the item
+  let items = document.querySelectorAll(".options-list-items");
+  items.forEach((item) => {
+  item.remove();
+  });
+}
 
- //check users answer
- ansSubmit.addEventListener('click', checkAns);
+function runGame(number) {
+  ansInput.value ='';
+  generateCountry (number);
+}
 
- //function to randomly generate a country
- function generateCountry () { 
+//function to check users answers
+function checkAns (event) {
+  event.preventDefault();
+  //only allow real capital cities from the object as options and check if answer correct.
+  if (!(Object.values(countryList).includes(ansInput.value))) {
+    alert('Sorry, not a valid option. Please section an answer from the dropdown list. Start typing your answer to bring up the dropdown menu.');
+  } else if (ansInput.value === Object.values(countryList)[num1]) {
+    alert('Well done. Correct.')
+    num1 = Math.floor(Math.random() * (Object.keys(countryList).length - 1));
+    runGame(num1);
+  } else if (ansInput.value !== Object.values(countryList)[num1]) {
+    alert(`Unlucky. The correct answer was ${Object.values(countryList)[num1]}`)
+    num1 = Math.floor(Math.random() * (Object.keys(countryList).length - 1));
+    runGame(num1);
+  }
+};
+
+
+
+//function to randomly generate a country
+function generateCountry (num) { 
   document.getElementById('answer').focus();
-
-  countryQ.innerHTML= Object.keys(countryList)[num1];
+  countryQ.innerHTML= Object.keys(countryList)[num];
 };
