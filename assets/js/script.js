e50912c96fc4183145d548196c900551b4d988f7
adammkeane <adamkeane.edu@gmail.com>
2022-10-30 21:22:57 +0000

    Add rough modal for how to pay instructions

--- assets/js/script.js
+++ assets/js/script.js
@@ -128,6 +128,9 @@ function populateList(e) {
     //Initially remove all elements ( so if user erases a letter or adds new letter then clean previous outputs)
   removeElements();
   ansSubmit.removeAttribute('disabled');
+  if ((e.key === 'Backspace' || e.key === 'Delete') && ansInput.value.length === 0) {
+    ansSubmit.setAttribute('disabled', 'true');
+  }
   //loop through above array
   for (let i in capitalArray) {
     //convert input to lowercase and compare with each string
@@ -149,14 +152,10 @@ function populateList(e) {
       document.querySelector('.options-list').appendChild(listItem);
     }
   }
-    if ((e.key === 'Backspace' || e.key === 'Delete') && ansInput.value.length === 0) {
-      ansSubmit.setAttribute('disabled', 'true');
-    }
+   
 };
 
-
 ansInput.addEventListener('keyup', populateList);
-ansInput.addEventListener('touchend', populateList);
 
 function displayNames(value) {
   ansInput.value = value;
@@ -272,5 +271,29 @@ function generateCountry (num) {
     countryQ.innerHTML= Object.keys(countryList)[num];
 };
 
+// Get the modal
+var modal = document.getElementById("myModal");
 
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
