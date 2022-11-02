44a746813477455e653b342fa25e00a9a0f76976
adammkeane <adamkeane.edu@gmail.com>
2022-11-02 00:30:29 +0000

    Improve modal styling

--- assets/js/asia-quiz.js
+++ assets/js/asia-quiz.js
@@ -267,3 +267,28 @@ function generateCountry (num) {
 };
 
 
+// Get the modal
+let modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+let btnNav = document.getElementById("my-btn-nav");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btnNav.onclick = function() {
+  modal.style.display = "flex";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
--- assets/js/home-page.js
+++ assets/js/home-page.js
@@ -1,16 +1,20 @@
 
 // Get the modal
-var modal = document.getElementById("myModal");
+let modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+let btn = document.getElementById("my-btn");
+let btnNav = document.getElementById("my-btn-nav");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+let span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
-  modal.style.display = "block";
+  modal.style.display = "flex";
+}
+btnNav.onclick = function() {
+  modal.style.display = "flex";
 }
 
 // When the user clicks on <span> (x), close the modal
