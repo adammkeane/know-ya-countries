2c753e6557185926dc080e19b7c57a41f83471de
adammkeane <adamkeane.edu@gmail.com>
2023-09-12 16:47:33 +0000

    Update styling on quiz pages

--- assets/js/caribbean-quiz.js
+++ assets/js/caribbean-quiz.js
@@ -275,30 +275,3 @@ function next(event) {
 function generateCountry(num) {
   countryQ.innerHTML = Object.keys(countryList)[num];
 }
-
-// Modal logic. Used code from https://www.w3schools.com/howto/howto_css_modals.asp
-// Get the modal
-let modal = document.getElementById("how-to-modal");
-
-// Get the button that opens the modal
-let btnNav = document.getElementById("modal-btn-nav");
-
-// Get the <span> element that closes the modal
-let span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btnNav.onclick = function () {
-  modal.style.display = "flex";
-};
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
-};
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
