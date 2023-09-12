e63dbabc6a7c230aefe0d71df9e59375fbc0a8a3
adammkeane <adamkeane.edu@gmail.com>
2023-09-12 17:58:29 +0000

    Improve quiz pages sizing

--- assets/js/asia-quiz.js
+++ assets/js/asia-quiz.js
@@ -211,7 +211,7 @@ function runGame(number) {
     replayButton.classList.add('btn');
     replayButton.classList.add('btn-outline-dark');
     replayButton.classList.add('mt-2');
-    replayButton.classList.add('p-2');
+    replayButton.classList.add('p-3');
     replayButton.type = 'submit';
     replayButton.id = 'replay-btn';
     replayButton.value = 'Replay';
