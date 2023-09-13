b155c966b55e1eba602cdc7f640fbcce8cefbc3e
adammkeane <adamkeane.edu@gmail.com>
2023-09-13 18:55:13 +0000

    Add us president 1-10 to the site

---
+++ assets/js/us-pres-quiz-1-10.js
@@ -0,0 +1,12 @@
+let countryList = {
+  '1 (April 1789 - March 1797)': 'George Washington',
+  '2 (March 1797 - March 1801)': 'John Adams',
+  '3 (March 1801 - March 1809)': 'Thomas Jefferson',
+  '4 (March 1809 - March 1817)': 'James Madison',
+  '5 (March 1817 - March 1825)': 'James Monroe',
+  '6 (March 1825 - March 1829)': 'John Quincy Adams',
+  '7 (March 1829 - March 1837)': 'Andrew Jackson',
+  '8 (March 1837 - March 1841)': 'Martin Van Buren',
+  '9 (March 1841 - April 1841)': 'William Henry Harrison',
+  '10 (April 1841 - March 1845)': 'John Tyler',
+};
