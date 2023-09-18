85e54ad2a7bbe7056ad51da6d19aae3ebb880e7d
adammkeane <adamkeane.edu@gmail.com>
2023-09-18 15:51:46 +0000

    Add north and central america quiz

---
+++ assets/js/america-quiz-n-c.js
@@ -0,0 +1,12 @@
+let countryList = {
+  'Belize': "Belmopan",
+  'Canada': 'Ottawa',
+  'Costa Rica': 'San José',
+  'El Salvador': 'San Salvador',
+  'Guatemala': 'Guatemala City',
+  'Honduras': 'Tegucigalpa',
+  'Mexico': "Mexico City",
+  'Nicaragua': 'Managua',
+  'Panama': 'Panama City',
+  'United States': 'Washington D.C.',
+};
