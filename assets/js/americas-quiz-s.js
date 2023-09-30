8be0bbc2598fcd368a231b7b42117a579e6309d9
adammkeane <adamkeane.edu@gmail.com>
2023-09-30 17:05:24 +0000

    Add south america js file

---
+++ assets/js/americas-quiz-s.js
@@ -0,0 +1,17 @@
+let countryList = {
+  'Argentina': "Buenos Aires",
+  'Bolivia': 'Sucre',
+  'Brazil': 'Brasília',
+  'Chile': 'Santiago',
+  'Columbia': 'Bogotá',
+  'Ecuador': 'Quito',
+  'Guyana': "Georgetown",
+  'Paraguay': 'Asunción',
+  'Peru': 'Lima',
+  'Suriname': 'Parimaribo',
+  'Uruguay': 'Montevideo',
+  'Venezuala': 'Caracas',
+};
+
+
+let capital = true;
